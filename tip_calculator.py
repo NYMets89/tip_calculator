db1826f4bd57389432efb37fe5a239abf4e34b18
Gregg Riess <businessman8994@gmail.com>
2021-10-06 18:11:03 -0400

    Corrected Cost of Meal Code

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,27 +2,27 @@
 ## Input will be converted to a float
 cost_of_food = float(input('Enter total cost of meal: '))
 
-#for cost in cost_of_food:
-#    if cost_of_food < 0.0:
-#        cost_of_food = float(input('Enter total cost of meal: '))
-
+while cost_of_food < 0.0:
+    cost_of_food = float(input('Please enter a positive number for cost of meal: '))
 
 ## Customer enters number of people splitting the final bill
-## Input will be converted to a float
+## Input will be an integer as you cannot have a fraction of a person
 number_of_people = float(input('Enter number of people splitting the bill: '))
+number_of_people = int(number_of_people)
 
 ## Customer enters what percent they want to tip
 ## Input will be converted to a float
 percentage_of_tip = float(input('Enter what percent you want to tip: ')) / 100
 
-## Convert customer tip percent into a percentage by dividing by 100
-#tip_percent = percentage_of_tip / 100
-## Set variable at fixed percent as a float
+## Set sales tax at fixed percent as a float
 sales_tax = 1.10
 
-##Calculate the total bill from customer including the tip
-total_bill = (cost_of_food * sales_tax) + (percentage_of_tip * cost_of_food)
+## Calculate the total bill from customer including the tip
+## Round function will make sure amount ends with no more than 2 decimal places
+total_bill = round((cost_of_food * sales_tax) + (percentage_of_tip * cost_of_food), 2)
+
 ## Amount each person owes including tip
-total_per_person = total_bill / number_of_people
+## Round function will make sure amount ends with no more than 2 decimal places
+total_per_person = round(total_bill / number_of_people, 2)
 
 print(f'The total bill including tip is {total_bill} and each person owes {total_per_person}')
